83797138f713b6cf9a33aaef6aee2b93d2604597
Neev Cohen <neevc.dev@gmail.com>
2023-11-26 21:48:01 +0200

    Work in progress - Implementing button handling

--- driver/led.h
+++ driver/led.h
@@ -11,6 +11,14 @@ struct led_device {
 			  *btn_1, 
 			  *btn_2, 
 			  *btn_3;
+	int irq_btn_0, 
+	    irq_btn_1, 
+	    irq_btn_2, 
+	    irq_btn_3;
+	unsigned long btn_0_last_press_jiffies,
+		      btn_1_last_press_jiffies,
+		      btn_2_last_press_jiffies,
+		      btn_3_last_press_jiffies;
 	struct proc_dir_entry *proc_file;
 	char data_buffer[DATA_BUFFER_SIZE];
 };
